dc391e8fda7a78d8ce4c5c0bafd34f4cec9d3c70
ucheannaanya <uchennaongit@gmail.com>
2024-02-15 19:25:35 +0100

    Add MUI, created component folder and navbar and footer components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <>
+    <header>
+        <ul>
+            <li>
+                <NavLink></NavLink>
+            </li>
+            <li>
+                <NavLink></NavLink>
+            </li>
+            <li>
+                <NavLink></NavLink>
+            </li>
+            <li>
+                <NavLink></NavLink>
+            </li>
+            <li>
+                <NavLink></NavLink>
+            </li>
+        </ul>
+    </header>
+    </>
+  )
+}
+
+export default Navbar
