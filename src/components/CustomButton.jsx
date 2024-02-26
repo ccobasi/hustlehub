9414f1c7c443c9eb970f26438572df6e167d1e2f
Chukwuma Obasi <ccobasi8@gmail.com>
2024-02-26 14:12:36 +0100

    change button color

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,9 +1,13 @@
 import { Button } from "@mui/material";
 
 export const CustomButton = ({ children }) => {
+  const buttonStyle = {
+    backgroundColor: "#87ceeb",
+   
+  };
   return (
     <>
-      <Button variant="contained">{children}</Button>
+      <Button variant="contained" style={buttonStyle}>{children}</Button>
     </>
   );
 };
