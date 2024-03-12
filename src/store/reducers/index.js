8d31348a2558c10a517e5bcb01313c1510c75d8c
Chukwuma Obasi <ccobasi8@gmail.com>
2024-03-12 10:18:29 +0100

    create forgot password form

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -4,12 +4,13 @@ import { combineReducers } from 'redux';
 import signUpReducer from './signupReducer';
 import signInReducer from './signinReducer';
 import changePasswordReducer from './changePasswordReducer';
+import forgotPasswordReducer from './forgotPasswordReducer';
 
 const rootReducer = combineReducers({
   signUp: signUpReducer,
   signIn: signInReducer,
   changePassword: changePasswordReducer,
-  // Add more reducers if needed
+  forgotPassword: forgotPasswordReducer,
 });
 
 export default rootReducer;
