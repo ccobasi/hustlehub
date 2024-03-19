0cfb37fec9221f1209b9eac0cdf363d42dac8171
Chukwuma Obasi <ccobasi8@gmail.com>
2024-03-19 08:55:23 +0100

    remove provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux';
-import store from './store/store.js';
+
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -12,11 +11,7 @@ import store from './store/store.js';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-  
+  <React.StrictMode>
     <App />
-  
-  </Provider>
-  // </React.StrictMode>,
+  </React.StrictMode>,
 )
