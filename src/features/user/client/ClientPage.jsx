0f53718e925abf22ba53e64a33d943964029f164
Chukwuma Obasi <ccobasi8@gmail.com>
2024-03-24 19:49:34 +0100

    fix signin bug

--- src/features/user/client/ClientPage.jsx
+++ src/features/user/client/ClientPage.jsx
@@ -1,5 +1,175 @@
+// import { Typography, Link, Stack, Container, Box } from "@mui/material";
+// // import { useNavigate } from "react-router-dom";
+// import { ClientFirstFeature } from "./Client";
+// import ClientSecondFeature from "./ProjectContainer";
+// import { useLocation } from "react-router-dom";
+
+// const ClientPage = () => {
+//   const data = useLocation();
+//   console.log(data);
+  
+//   // let navigate = useNavigate();
+
+//   return (
+//     <>
+//       {/* Container for the Client page */}
+//       <Container component="main" maxWidth="lg">
+//         {/*First Client Feature*/}
+//         <ClientFirstFeature />
+
+//         {/*First Heading*/}
+//         <Typography
+//           variant="h6"
+//           sx={{
+//             fontFamily: "Poppins",
+//             fontWeight: "600",
+//             fontSize: "16px",
+//             lineHeight: "20.8px",
+//             textAlign: "start",
+
+//             color: (theme) =>
+//               theme.palette.mode === "light"
+//                 ? theme.palette.primary.lightModeHeroTitle
+//                 : theme.palette.primary.darkModeHeroTitle,
+
+//             pt: "20px",
+
+//             ml: "15%",
+//           }}
+//         >
+//           My Projects
+//           <Link
+//             href="/create-project"
+//             sx={{
+//               fontFamily: "Poppins",
+//               fontWeight: "400",
+//               fontSize: "13px",
+//               lineHeight: "20.8px",
+
+//               textDecoration: "none",
+
+//               color: "#87CEEB",
+//               ml: "40%",
+//             }}
+//           >
+//             Create a Project
+//           </Link>
+//         </Typography>
+
+//         {/*Second Client Feature*/}
+//         <ClientSecondFeature />
+
+//         {/*Second Heading*/}
+//         <Box sx={{ mt: "5%" }}>
+//           <Link
+//             href="/categories"
+//             sx={{
+//               textDecoration: "none",
+//               ml: "67%",
+//               color: "#87CEEB",
+//               fontFamily: "Poppins",
+//               fontWeight: "500",
+//               fontSize: "12px",
+//               lineHeight: "19.2px",
+//               letterSpacing: "-1%",
+//             }}
+//           >
+//             View All Project
+//           </Link>
+//         </Box>
+
+//         {/*Third Heading*/}
+
+//         <Stack direction="row">
+//           <Typography
+//             variant="h6"
+//             sx={{
+//               fontFamily: "Poppins",
+//               fontWeight: "600",
+//               fontSize: "16px",
+//               lineHeight: "20.8px",
+              
+
+//               color: (theme) =>
+//                 theme.palette.mode === "light"
+//                   ? theme.palette.primary.lightModeHeroTitle
+//                   : theme.palette.primary.darkModeHeroTitle,
+
+//               ml: "15%",
+//               mt: "2%",
+//             }}
+//           >
+//             Payment History
+//           </Typography>
+
+//           <Link
+//             href="/about/question/21334565"
+//             sx={{
+//               textDecoration: "none",
+//               ml: "33%",
+//               mt: "2%",
+//               color: "#95969D",
+
+//               fontFamily: "Poppins",
+//               fontWeight: "400",
+//               fontSize: "13px",
+//               lineHeight: "20.8px",
+//             }}
+//           >
+//             See all
+//           </Link>
+//         </Stack>
+
+//         {/*Fourth Heading*/}
+
+//         <Stack direction="row" sx={{ mt: "20%" }}>
+//           <Typography
+//             variant="h6"
+//             sx={{
+//               fontFamily: "Poppins",
+//               fontWeight: "600",
+//               fontSize: "16px",
+//               lineHeight: "20.8px",
+              
+
+//               color: (theme) =>
+//                 theme.palette.mode === "light"
+//                   ? theme.palette.primary.lightModeHeroTitle
+//                   : theme.palette.primary.darkModeHeroTitle,
+
+//               ml: "15%",
+//               mt: "2%",
+//             }}
+//           >
+//             Messages
+//           </Typography>
+
+//           <Link
+//             href="/about/question/21334565"
+//             sx={{
+//               textDecoration: "none",
+//               ml: "39.5%",
+//               mt: "2%",
+//               color: "#95969D",
+
+//               fontFamily: "Poppins",
+//               fontWeight: "400",
+//               fontSize: "13px",
+//               lineHeight: "20.8px",
+//             }}
+//           >
+//             See all
+//           </Link>
+//         </Stack>
+//       </Container>
+//     </>
+//   );
+// };
+
+// export default ClientPage;
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import { Typography, Link, Stack, Container, Box } from "@mui/material";
-// import { useNavigate } from "react-router-dom";
 import { ClientFirstFeature } from "./Client";
 import ClientSecondFeature from "./ProjectContainer";
 import { useLocation } from "react-router-dom";
@@ -7,17 +177,12 @@ import { useLocation } from "react-router-dom";
 const ClientPage = () => {
   const data = useLocation();
   console.log(data);
-  
-  // let navigate = useNavigate();
 
   return (
-    <>
-      {/* Container for the Client page */}
-      <Container component="main" maxWidth="lg">
-        {/*First Client Feature*/}
-        <ClientFirstFeature />
+    <Container component="main" maxWidth="lg">
+      <ClientFirstFeature />
 
-        {/*First Heading*/}
+      <div>
         <Typography
           variant="h6"
           sx={{
@@ -26,14 +191,11 @@ const ClientPage = () => {
             fontSize: "16px",
             lineHeight: "20.8px",
             textAlign: "start",
-
             color: (theme) =>
               theme.palette.mode === "light"
                 ? theme.palette.primary.lightModeHeroTitle
                 : theme.palette.primary.darkModeHeroTitle,
-
             pt: "20px",
-
             ml: "15%",
           }}
         >
@@ -45,9 +207,7 @@ const ClientPage = () => {
               fontWeight: "400",
               fontSize: "13px",
               lineHeight: "20.8px",
-
               textDecoration: "none",
-
               color: "#87CEEB",
               ml: "40%",
             }}
@@ -55,114 +215,98 @@ const ClientPage = () => {
             Create a Project
           </Link>
         </Typography>
+      </div>
 
-        {/*Second Client Feature*/}
-        <ClientSecondFeature />
+      <ClientSecondFeature />
 
-        {/*Second Heading*/}
-        <Box sx={{ mt: "5%" }}>
-          <Link
-            href="/categories"
-            sx={{
-              textDecoration: "none",
-              ml: "67%",
-              color: "#87CEEB",
-              fontFamily: "Poppins",
-              fontWeight: "500",
-              fontSize: "12px",
-              lineHeight: "19.2px",
-              letterSpacing: "-1%",
-            }}
-          >
-            View All Project
-          </Link>
-        </Box>
+      <Box sx={{ mt: "5%" }}>
+        <Link
+          href="/categories"
+          sx={{
+            textDecoration: "none",
+            ml: "67%",
+            color: "#87CEEB",
+            fontFamily: "Poppins",
+            fontWeight: "500",
+            fontSize: "12px",
+            lineHeight: "19.2px",
+            letterSpacing: "-1%",
+          }}
+        >
+          View All Project
+        </Link>
+      </Box>
 
-        {/*Third Heading*/}
+      <Stack direction="row">
+        <Typography
+          variant="h6"
+          sx={{
+            fontFamily: "Poppins",
+            fontWeight: "600",
+            fontSize: "16px",
+            lineHeight: "20.8px",
+            color: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.primary.lightModeHeroTitle
+                : theme.palette.primary.darkModeHeroTitle,
+            ml: "15%",
+            mt: "2%",
+          }}
+        >
+          Payment History
+        </Typography>
+        <Link
+          href="/about/question/21334565"
+          sx={{
+            textDecoration: "none",
+            ml: "33%",
+            mt: "2%",
+            color: "#95969D",
+            fontFamily: "Poppins",
+            fontWeight: "400",
+            fontSize: "13px",
+            lineHeight: "20.8px",
+          }}
+        >
+          See all
+        </Link>
+      </Stack>
 
-        <Stack direction="row">
-          <Typography
-            variant="h6"
-            sx={{
-              fontFamily: "Poppins",
-              fontWeight: "600",
-              fontSize: "16px",
-              lineHeight: "20.8px",
-              
-
-              color: (theme) =>
-                theme.palette.mode === "light"
-                  ? theme.palette.primary.lightModeHeroTitle
-                  : theme.palette.primary.darkModeHeroTitle,
-
-              ml: "15%",
-              mt: "2%",
-            }}
-          >
-            Payment History
-          </Typography>
-
-          <Link
-            href="/about/question/21334565"
-            sx={{
-              textDecoration: "none",
-              ml: "33%",
-              mt: "2%",
-              color: "#95969D",
-
-              fontFamily: "Poppins",
-              fontWeight: "400",
-              fontSize: "13px",
-              lineHeight: "20.8px",
-            }}
-          >
-            See all
-          </Link>
-        </Stack>
-
-        {/*Fourth Heading*/}
-
-        <Stack direction="row" sx={{ mt: "20%" }}>
-          <Typography
-            variant="h6"
-            sx={{
-              fontFamily: "Poppins",
-              fontWeight: "600",
-              fontSize: "16px",
-              lineHeight: "20.8px",
-              
-
-              color: (theme) =>
-                theme.palette.mode === "light"
-                  ? theme.palette.primary.lightModeHeroTitle
-                  : theme.palette.primary.darkModeHeroTitle,
-
-              ml: "15%",
-              mt: "2%",
-            }}
-          >
-            Messages
-          </Typography>
-
-          <Link
-            href="/about/question/21334565"
-            sx={{
-              textDecoration: "none",
-              ml: "39.5%",
-              mt: "2%",
-              color: "#95969D",
-
-              fontFamily: "Poppins",
-              fontWeight: "400",
-              fontSize: "13px",
-              lineHeight: "20.8px",
-            }}
-          >
-            See all
-          </Link>
-        </Stack>
-      </Container>
-    </>
+      <Stack direction="row" sx={{ mt: "20%" }}>
+        <Typography
+          variant="h6"
+          sx={{
+            fontFamily: "Poppins",
+            fontWeight: "600",
+            fontSize: "16px",
+            lineHeight: "20.8px",
+            color: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.primary.lightModeHeroTitle
+                : theme.palette.primary.darkModeHeroTitle,
+            ml: "15%",
+            mt: "2%",
+          }}
+        >
+          Messages
+        </Typography>
+        <Link
+          href="/about/question/21334565"
+          sx={{
+            textDecoration: "none",
+            ml: "39.5%",
+            mt: "2%",
+            color: "#95969D",
+            fontFamily: "Poppins",
+            fontWeight: "400",
+            fontSize: "13px",
+            lineHeight: "20.8px",
+          }}
+        >
+          See all
+        </Link>
+      </Stack>
+    </Container>
   );
 };
 
--- src/features/user/client/ClientProfile.jsx
+++ src/features/user/client/ClientProfile.jsx
@@ -1,24 +1,51 @@
+// // eslint-disable-next-line no-unused-vars
+// import * as React from "react";
+// import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+// import { useLocation } from "react-router-dom";
+// import Img from "../../../assets/mask-group-8wG.png"
+
+// const ClientProfile = ({ name, jobTitle, image, imageLabel }) => {
+//     const data = useLocation();
+//   console.log(data);
+//     jobTitle = "Project Manager"
+//   return (
+//     <Card sx={{ maxWidth: "345", mt: 12, ml: "42%"}}>
+//       <CardMedia
+//         component="img"
+//         height="120"
+//         image={Img}
+//         alt={imageLabel}
+//       />
+//       <CardContent>
+//         <Typography gutterBottom variant="h6" component="div">
+//           {data.state}
+//         </Typography>
+//         <Typography variant="body2" color="text.secondary">
+//           {jobTitle}
+//         </Typography>
+//       </CardContent>
+//     </Card>
+//   );
+// };
+
+// export default ClientProfile;
 // eslint-disable-next-line no-unused-vars
 import * as React from "react";
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
-import Img from "../../../assets/mask-group-8wG.png"
+import Img from "../../../assets/mask-group-8wG.png";
 
 const ClientProfile = ({ name, jobTitle, image, imageLabel }) => {
-    const data = useLocation();
+  const data = useLocation();
   console.log(data);
-    jobTitle = "Project Manager"
+  jobTitle = "Project Manager";
+
   return (
-    <Card sx={{ maxWidth: "345", mt: 12, ml: "42%"}}>
-      <CardMedia
-        component="img"
-        height="120"
-        image={Img}
-        alt={imageLabel}
-      />
+    <Card sx={{ maxWidth: "345", mt: 12, ml: "42%" }}>
+      <CardMedia component="img" height="120" image={Img} alt={imageLabel} />
       <CardContent>
         <Typography gutterBottom variant="h6" component="div">
-          {data.state}
+          {name} {/* Provide the 'name' prop here */}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {jobTitle}
