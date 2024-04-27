8ee700a6fc09627019e6096352ea804d85bfc8f4
Chukwuma Obasi <ccobasi8@gmail.com>
2024-04-27 08:53:22 +0100

    remove user model

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
+from .manager import UserManager
 
 
 ROLE_CHOICES = (
@@ -8,32 +9,8 @@ ROLE_CHOICES = (
     ('client', 'Client'),
 )
 
-class User(AbstractUser, PermissionsMixin):
-    email=models.EmailField(max_length=255, verbose_name=('Email Address'), unique=True)
-    first_name = models.CharField(verbose_name=("First Name"), max_length=100)
-    last_name = models.CharField(verbose_name=("Last Name"), max_length=100)
-    is_superuser = models.BooleanField(default=False)
-    is_verified=models.BooleanField(default=False)
-    is_staff=models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = "email"
 
-    REQUIRED_FIELDS = ["first name", "last name"]
-
-    objects = UserManager()
-
-    def __str__(self):
-        return self.email
-    
-    @property
-    def get_full_name(self):
-        return f"{self.first_name} {self.last_name}"
-    
-    def token(self):
-        pass
 
 class CustomUser(AbstractUser):
     full_name = models.CharField(max_length=255, blank=True)
