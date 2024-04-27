ff49580371192873e2e34a8b4df16133a0cbb53d
Chukwuma Obasi <ccobasi8@gmail.com>
2024-04-27 07:15:20 +0100

    create manager and env

--- backend/user/views.py
+++ backend/user/views.py
@@ -5,6 +5,7 @@ from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
