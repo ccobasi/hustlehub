a40f61929a3474e4533d94b07c1a8cf51adbad2d
Chukwuma Obasi <ccobasi8@gmail.com>
2024-04-28 21:15:38 +0100

    add email verify method

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from .views import *
-from user.views import RegisterUserView
+from user.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'user'
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('verify-email/', VerifyUserEmail.as_view(), name='verify'),
     path("token/", TokenObtainPairView.as_view(), name="get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("user-auth", include("rest_framework.urls"))
--- backend/user/views.py
+++ backend/user/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from .utils import send_code_to_user
 from django.db import IntegrityError
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .onetimepassword import OneTimePassword
 
 
 
@@ -30,4 +31,21 @@ class RegisterUserView(GenericAPIView):
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+class VerifyUserEmail(GenericAPIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        otpcode=request.data.get('otp')
+        try:
+            user_code_obj=OneTimePassword.objects.get(code=otpcode)
+            user=user_code_obj.user
+            if not user.is_verified:
+                user.is_verified=True
+                user.save()
+                return Response({
+                    "message":'Your email account has been verified successfully!'
+                }, status=status.HTTP_200_OK)
+            return Response({
+                'message': 'Code is invalid user already verified.'
+            }, status=status.HTTP_204_NO_CONTENT)
+        except  OneTimePassword.DoesNotExist:
+            return Response({'message': 'passcode not provided'}, status=status.HTTP_404_NOT_FOUND)
