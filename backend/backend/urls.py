74c4aaefeff98a22c3a0647effb86d8cbd7491d2
Chukwuma Obasi <ccobasi8@gmail.com>
2024-05-01 17:45:42 +0100

    create urls for social account

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls', namespace='user'))
+    path('user/', include('user.urls', namespace='user')),
+    path('social_account/', include('social_account.urls', namespace='social_account'))
 ]
