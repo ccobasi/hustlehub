829add51452540865647e046d0281eeffd01c226
Chukwuma Obasi <ccobasi8@gmail.com>
2024-05-01 15:34:00 +0100

    create social account serializers

--- backend/social_account/utils.py
+++ backend/social_account/utils.py
@@ -15,3 +15,38 @@ class Google():
                 return id_info
         except Exception as e:
             return "token is invalid or expired"
+        
+
+def login_social_user(email, password):
+    user=authenticate(email=email, password=password)
+    user_tokens=user.tokens()
+
+    return {
+        'email': user.email, 
+        'full_name': user.get_full_name,
+        'access_token': str(user_tokens.get('access')),
+        'refresh_token': str(user_tokens.get('refresh')),
+    }
+
+
+def register_social_user(provider, email, first_name,  last_name):
+    user=User.objects.filter(email=email)
+    if user.exists():
+        if provider == user[0].auth_provider:
+            login_social_user(email, settings.SOCIAL_AUTH_PASSWORD)
+        else:
+            raise AuthenticationFailed(
+                detail=f"please continue your login with {user[0].auth_provider}"
+            )
+    else:
+        new_user={
+        'email': email,
+        'first_name': first_name,
+        'last_name': last_name,
+        'password':settings.SOCIAL_AUTH_PASSWORD
+        }
+        register_user=User.objects.create_user(**new_user)
+        register_user.auth_provider = provider
+        register_user.is_verified=True
+        register_user.save()
+        login_social_user(email=register_user.email, password=settings.SOCIAL_AUTH_PASSWORD)
