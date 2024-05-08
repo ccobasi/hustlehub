0a161b85c78c59b481f4f6ae1cc64249034df8b5
Chukwuma Obasi <ccobasi8@gmail.com>
2024-05-08 06:06:11 +0100

    conditionally render user profile edit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
