42d107617b7e196180a17e5f8f5485336fefc5c4
Chukwuma Obasi <ccobasi8@gmail.com>
2024-05-26 21:27:01 +0100

    create proposal app and define proposal model

---
+++ backend/proposal/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from user.models import User
+from .models import Project
+
+PROPOSAL_STATUS_CHOICES = (
+    ('pending', 'Pending'),
+    ('accepted', 'Accepted'),
+    ('rejected', 'Rejected'),
+)
+
+
+class Proposal(models.Model):
+ 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="proposals")
+    freelancer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="freelancer_proposals")
+    proposed_rate = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("Proposed Rate"))
+    estimated_days = models.IntegerField(verbose_name=_("Estimated Completion Days"))
+    cover_letter = models.TextField(verbose_name=_("Cover Letter"))
+    submitted_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Date Submitted"))
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, choices=PROPOSAL_STATUS_CHOICES, default="pending", verbose_name=_("Proposal Status"))
+
+    def __str__(self):
+        return f"{self.freelancer.get_full_name()} - {self.project.title}"
+
