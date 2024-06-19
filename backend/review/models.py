01684ecc8193b587a95f59a4c687ea0d7e8e2aae
Chukwuma Obasi <ccobasi8@gmail.com>
2024-06-19 20:52:22 +0100

    create review app

---
+++ backend/review/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+from .models import Contract
+from user.models import User
+
+class Review(models.Model):
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name='reviews')
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review for {self.contract} by {self.reviewer}'
+
