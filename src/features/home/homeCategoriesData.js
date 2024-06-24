520df9999cbfd8ebc5b54568e392dfd438075935
Chukwuma Obasi <ccobasi8@gmail.com>
2024-06-24 17:06:53 +0100

    create categorys component

--- src/features/home/homeCategoriesData.js
+++ src/features/home/homeCategoriesData.js
@@ -1,62 +1,134 @@
 const  homeCategoriesData = [
     {
         id: 1,
-      jobTitle: "Writing & Translation",
+      jobTitle: "Accounting & Finance",
       sourceSet:
-        "/assets/rectangle-114-yQi.png, /assets/rectangle-114-yQ.png, /assets/rectangle-114-zCe.png",
-      image: "/assets/rectangle-114-zCe.png",
+        "/assets/accountingAndFinance.png, /assets/accountingAndFinance.png, /assets/accountingAndFinance.png",
+      image: "/assets/accountingAndFinance.png",
   
-      imageLabel: "Writing & Translation",
+      imageLabel: "Accounting & Finance",
     },
   
     {
         id: 2,
-      jobTitle: "Programming & Tech",
+      jobTitle: "Art & Design",
       sourceSet:
-        "/assets/programmingAndTechSm.png, /assets/programmingAndTechLg.png",
-      image: "/assets/programmingAndTechSm.png",
+        "/assets/artAndDesign.png, /assets/artAndDesign.png",
+      image: "/assets/artAndDesign.png",
   
       imageLabel: "Programming & Tech",
     },
     {
       id: 3,
-    jobTitle: "Software Engineer",
+    jobTitle: "Administration & Customer Support",
     sourceSet:
-      "/assets/softwareEngineerSm.png, /assets/softwareEngineerLg.png",
-    image: "/assets/softwareEngineerLg.png",
+      "/assets/adminAndSupport.png, /assets/adminAndSupport.png",
+    image: "/assets/adminAndSupport.png",
 
-    imageLabel: "Software Engineer",
+    imageLabel: "Administration & Customer Support",
   },
-  
-    {
-        id: 4,
-      jobTitle: "Business & Finance",
-      sourceSet:
-        "/assets/businessAndFinanceSm.png, /assets/businessAndFinanceLg.png",
-      image: "/assets/businessAndFinanceLg.png",
-  
-      imageLabel: "Business & Finance",
-    },
+  {
+    id: 4,
+  jobTitle: "Business & Management",
+  sourceSet:
+    "/assets/businessAndMgt.png, /assets/businessAndMgt.png",
+  image: "/assets/businessAndMgt.png",
 
+  imageLabel: "Business & Management ",
+},
+  
     {
         id: 5,
-      jobTitle: "Electrical Technician",
+      jobTitle: "Agriculture",
       sourceSet:
-        "/assets/businessAndFinanceSm.png, /assets/businessAndFinanceLg.png",
-      image: "/assets/businessAndFinanceLg.png",
+        "/assets/agriculture.png, /assets/agriculture.png",
+      image: "/assets/agriculture.png",
   
-      imageLabel: "Electrical Technician",
+      imageLabel: "Agriculture ",
     },
 
-    {
-        id: 6,
-      jobTitle: "Civil Engineer",
-      sourceSet:
-        "/assets/businessAndFinanceSm.png, /assets/businessAndFinanceLg.png",
-      image: "/assets/businessAndFinanceLg.png",
-  
-      imageLabel: "Civil Engineer",
-    },
+   
+  {
+    id: 6,
+  jobTitle: "Cleaning Services",
+  sourceSet:
+    "/assets/cleaningServices.png, /assets/cleaningServices.png",
+  image: "/assets/cleaningServices.png",
+
+  imageLabel: "Cleaning Services",
+},
+{
+  id: 7,
+jobTitle: "Computer & IT",
+sourceSet:
+  "/assets/computerAndIT.png, /assets/computerAndIT.png",
+image: "/assets/computerAndIT.png",
+
+imageLabel: "Computer & IT ",
+},
+{
+  id: 8,
+jobTitle: "Food & Hospitality",
+sourceSet:
+  "/assets/foodAndHospitality.png, /assets/foodAndHospitality.png",
+image: "/assets/foodAndHospitality.png",
+
+imageLabel: "Food & Hospitality ",
+},
+{
+  id: 9,
+jobTitle: "Marketing & ContentCreation",
+sourceSet:
+  "/assets/marketingAndContentCreation.png, /assets/marketingAndContentCreation.png",
+image: "/assets/marketingAndContentCreation.png",
+
+imageLabel: "Marketing & ContentCreation ",
+},
+{
+  id: 10,
+jobTitle: "Education",
+sourceSet:
+  "/assets/education.png, /assets/education.png",
+image: "/assets/education.png",
+
+imageLabel: "Education ",
+},
+{
+  id: 11,
+jobTitle: "Healthcare",
+sourceSet:
+  "/assets/healthcare.png, /assets/healthcare.png",
+image: "/assets/healthcare.png",
+
+imageLabel: "Healthcare ",
+},
+{
+  id: 12,
+jobTitle: "Media & Entertainment",
+sourceSet:
+  "/assets/mediaAndEntertainment.png, /assets/mediaAndEntertainment.png",
+image: "/assets/mediaAndEntertainment.png",
+
+imageLabel: "Media & Entertainment ",
+},
+{
+  id: 13,
+jobTitle: "Engineer & Architecture",
+sourceSet:
+  "/assets/engineerAndArchitecture.png, /assets/engineerAndArchitecture.png",
+image: "/assets/engineerAndArchitecture.png",
+
+imageLabel: "Engineer & Architecture ",
+},
+{
+  id: 14,
+jobTitle: "Human Resource",
+sourceSet:
+  "/assets/humanResource.png, /assets/humanResource.png",
+image: "/assets/humanResource.png",
+
+imageLabel: "Human Resource ",
+},
   ];
 
   export default homeCategoriesData;
