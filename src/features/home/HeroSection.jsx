9b1f17a85e32c539e165bd324abc64acfea82ae1
Chukwuma Obasi <ccobasi8@gmail.com>
2024-06-24 16:12:36 +0100

    Update hero section

--- src/features/home/HeroSection.jsx
+++ src/features/home/HeroSection.jsx
@@ -1,11 +1,18 @@
 import HeroCard from './HeroCard';
 import heroData from './heroData';
-import { Grid } from "@mui/material";
+import { Grid,Button } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+
 
 export const HeroSection = () => {
+  const navigate = useNavigate();
   let secondContainer = heroData.map((el) => {
     return <HeroCard key={el.id} {...el} />;
   });
+
+  const handleClick = () => {
+    navigate('/sign-up'); 
+  }
   return (
     <>
       {/* Grid for the Categories Feature*/}
@@ -21,6 +28,29 @@ export const HeroSection = () => {
         {secondContainer}
       </Grid>
       {/* Grid End*/}
+     
+       {/* Button for Getting started */}
+       <Button
+          variant="contained"
+          sx={{
+            mb: "5%",
+            typography: (theme) => theme.typography.heroGetStartedButton,
+            "&:hover": {
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[400]
+                  : theme.palette.grey[500],
+            },
+            ml:"55%",
+            mt:"4%"
+          
+          }}
+          onClick={handleClick}
+        >
+          Get Started
+        </Button>
+        {/* Button End */}
+
     </>
   );
 };
